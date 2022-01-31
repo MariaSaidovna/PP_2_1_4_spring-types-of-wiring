f1405664fe10ec07ce19ab3084aee27db95af77d
Maria <mashytkamickey@mail.ru>
2022-01-31 15:32:24 +0300

    Spring types of wiring

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,19 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+
+    private String deth = "!";
+
+    public void setDeth(String deth) {
+        this.deth = deth;
+    }
+
+   @Override
+    public String toString() {
+        return deth.toString();
+    }
+
 }
